e8296319b6f933fb21d8a463dccb8a6d31c94d72
sekrit-twc <noreply@example.com>
2017-06-10 11:14:31 -0700

    Add benchmark tool.

---
+++ benchmark/main.cpp
@@ -0,0 +1,72 @@
+#include <chrono>
+#include <cstdio>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include "cube.h"
+#include "lut.h"
+
+int main(int argc, char **argv)
+{
+	unsigned niter = 2000000;
+	bool simd = true;
+
+	try {
+		if (argc >= 2)
+			niter = std::stoi(argv[1]);
+		if (argc >= 3)
+			simd = !!std::stoi(argv[2]);
+	} catch (const std::exception &) {
+		std::cerr << "usage: benchmark [niter] [simd]\n";
+		return 1;
+	}
+
+	timecube::Cube cube;
+	cube.lut = {
+		0.0f, 0.0f, 0.0f, // R0 G0 B0
+		1.0f, 0.0f, 0.0f, // R1 G0 B0
+		0.0f, 1.0f, 0.0f, // R0 G1 B0
+		1.0f, 1.0f, 0.0f, // R1 G1 B0
+		0.0f, 0.0f, 1.0f, // R0 G0 B1
+		1.0f, 0.0f, 1.0f, // R1 G0 B1
+		0.0f, 1.0f, 1.0f, // R0 G1 B1
+		1.0f, 1.0f, 1.0f, // R1 G1 B1
+	};
+	cube.is_3d = true;
+	cube.n = 2;
+
+	std::unique_ptr<timecube::Lut> lut;
+
+	try {
+		if (!(lut = timecube::create_lut_impl(cube, simd)))
+			throw std::runtime_error{ "failed to create LUT implementation" };
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << '\n';
+	}
+
+	alignas(32) float r[1024] = { 0 };
+	alignas(32) float g[1024] = { 0 };
+	alignas(32) float b[1024] = { 0 };
+
+	const void *src[3] = { r, g, b };
+	void *dst[3] = { r, g, b };
+
+	auto start = std::chrono::high_resolution_clock::now();
+	for (unsigned i = 0; i < niter; ++i) {
+		lut->process(src, dst, sizeof(r) / sizeof(r[0]));
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+
+	double duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+	double pels_per_s = (sizeof(r) / sizeof(r[0])) * niter / duration;
+	double ns_per_pel = 1e9 / pels_per_s;
+	std::cout << "elapsed: " << duration << " (" << ns_per_pel << " ns/pel)\n";
+
+	if (std::getenv("INTERACTIVE")) {
+		std::cout << "press any key to continue...\n";
+		std::fgetc(stdin);
+	}
+
+	return 0;
+}
--- test/main.cpp
+++ test/main.cpp
@@ -4,43 +4,56 @@
 #include "cube.h"
 #include "lut.h"
 
+namespace {
+
+void usage()
+{
+	std::cerr << "usage: test cubefile [x y z] [simd]\n";
+}
+
+} // namespace
+
+
 int main(int argc, char **argv)
 {
 	if (argc < 2) {
-		std::cerr << "usage: test cubefile [x y z]\n";
+		usage();
 		return 1;
 	}
 
-	timecube::Cube cube;
+	std::unique_ptr<timecube::Lut> lut;
+	float x = 0.0f;
+	float y = 0.0f;
+	float z = 0.0f;
+	bool simd = false;
 
 	try {
-		cube = timecube::read_cube_from_file(argv[1]);
-		std::cout << "first entry: " << cube.lut[0] << ' ' << cube.lut[1] << ' ' << cube.lut[2] << '\n';
-		std::cout << "last entry: " << cube.lut[cube.lut.size() - 3] << ' ' << cube.lut[cube.lut.size() - 2] << ' ' << cube.lut[cube.lut.size() - 1] << '\n';
-	} catch (const std::exception &e) {
-		std::cerr << "failed to open CUBE: " << e.what() << '\n';
-		return 1;
-	}
-
-	if (argc < 5)
-		return 0;
-
-	try {
-		std::unique_ptr<timecube::Lut> lut = timecube::create_lut_impl(cube, false);
-		float x, y, z;
-
-		if (!lut)
-			throw std::runtime_error{ "failed to create LUT implementation" };
-
-		try {
+		if (argc >= 5) {
 			x = std::stof(argv[2]);
 			y = std::stof(argv[3]);
 			z = std::stof(argv[4]);
-		} catch (const std::exception &) {
-			std::cerr << "expected 3 numbers\n";
-			return 1;
 		}
 
+		if (argc >= 6)
+			simd = !!std::stof(argv[5]);
+	} catch (const std::exception &) {
+		usage();
+		return 1;
+	}
+
+	try {
+		timecube::Cube cube = timecube::read_cube_from_file(argv[1]);
+		std::cout << "first entry: " << cube.lut[0] << ' ' << cube.lut[1] << ' ' << cube.lut[2] << '\n';
+		std::cout << "last entry: " << cube.lut[cube.lut.size() - 3] << ' ' << cube.lut[cube.lut.size() - 2] << ' ' << cube.lut[cube.lut.size() - 1] << '\n';
+
+		if (!(lut = timecube::create_lut_impl(cube, simd)))
+			throw std::runtime_error{ "failed to create LUT implementation" };
+	} catch (const std::exception &e) {
+		std::cerr << "failed to load CUBE: " << e.what() << '\n';
+		return 1;
+	}
+
+	try {
 		const void *src[3] = { &x, &y, &z };
 		void *dst[3] = { &x, &y, &z };
 
