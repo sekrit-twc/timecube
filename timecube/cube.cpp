7b456169c9d81671ac975da86dfcdc5822641000
sekrit-twc <noreply@example.com>
2026-04-13 20:56:31 -0700

    Use std::string_view

--- timecube/cube.cpp
+++ timecube/cube.cpp
@@ -1,12 +1,13 @@
 #include <cctype>
 #include <cerrno>
+#include <charconv>
 #include <cstddef>
 #include <cstdio>
 #include <cstring>
-#include <locale>
 #include <memory>
-#include <sstream>
 #include <stdexcept>
+#include <string>
+#include <string_view>
 #include <system_error>
 #include "cube.h"
 
@@ -14,6 +15,8 @@
   #include <string>
   #include <Windows.h>
 #endif
+
+using namespace std::string_view_literals;
 
 namespace timecube {
 namespace {
@@ -42,60 +45,48 @@ void read_line(char *buf, FILE *f)
 	} while (buf[0] == '#' || buf[0] == '\n');
 }
 
-const char *skip_space(const char *buf)
+std::string_view skip_space(std::string_view buf)
 {
-	while (*buf && (*buf == ' ' || *buf == '\t')) {
-		++buf;
-	}
-	return buf;
+	return buf.substr(std::min(buf.find_first_not_of(" \t"), buf.size()));
 }
 
 template <class T>
-const char *parse_number(const char *buf, T *dst)
+std::string_view parse_number(std::string_view buf, T *dst)
 {
-	const char *first = buf;
+	std::size_t end_idx = std::min(buf.find_first_of(" \t\n"), buf.size());
 
-	while (*buf != '\0' && *buf != ' ' && *buf != '\t' && *buf != '\n') {
-		++buf;
-	}
-
-	std::istringstream ss{ std::string{ first, buf } };
-	ss.imbue(std::locale::classic());
-
-	char eof_test;
-	if (!(ss >> *dst) || (ss >> eof_test))
+	std::from_chars_result res = std::from_chars(&*buf.begin(), &*buf.begin() + end_idx, *dst);
+	if (res.ec != std::error_code{} || res.ptr != &*buf.begin() + end_idx)
 		throw std::runtime_error{ "invalid number" };
 
-	return buf;
+	return buf.substr(end_idx);
 }
 
-bool is_keyword(const char *buf, const char *keyword)
+bool is_keyword(std::string_view buf, std::string_view keyword)
 {
-	size_t len = std::strlen(keyword);
-	return !std::strncmp(buf, keyword, len) && (buf[len] == ' ' || buf[len] == '\t' || buf[len] == '\n');
+	size_t kw_size = keyword.size();
+	return buf.size() > kw_size && buf.substr(0, kw_size) == keyword &&
+		(buf[kw_size] == ' ' || buf[kw_size] == '\t' || buf[kw_size] == '\n');
 }
 
-std::string parse_title(const char *buf)
+std::string parse_title(std::string_view buf)
 {
-	const char *first;
-
-	buf += std::strlen("TITLE");
+	buf = buf.substr("TITLE"sv.size());
 	buf = skip_space(buf);
 
-	if (*buf++ != '"')
+	if (buf.empty() || buf.front() != '"')
 		throw std::runtime_error{ "missing opening quote in TITLE" };
 
-	first = buf;
-
-	if (!(buf = std::strchr(buf, '"')))
+	size_t end = buf.find('"', 1);
+	if (end == std::string_view::npos)
 		throw std::runtime_error{ "missing closing quote in TITLE" };
 
-	return{ first, buf };
+	return std::string{ buf.substr(1, end) };
 }
 
-void parse_domain_minmax(const char *buf, float dst[3])
+void parse_domain_minmax(std::string_view buf, float dst[3])
 {
-	buf += std::strlen("DOMAIN_MIN");
+	buf = buf.substr("DOMAIN_MIN"sv.size());
 	buf = skip_space(buf);
 
 	buf = parse_number(buf, dst + 0);
@@ -105,18 +96,18 @@ void parse_domain_minmax(const char *buf, float dst[3])
 	buf = parse_number(buf, dst + 2);
 }
 
-uint_least32_t parse_lut_size(const char *buf)
+uint_least32_t parse_lut_size(std::string_view buf)
 {
 	uint_least32_t n;
 
-	buf += std::strlen("LUT_1D_SIZE");
+	buf = buf.substr("LUT_1D_SIZE"sv.size());
 	buf = skip_space(buf);
 	buf = parse_number(buf, &n);
 
 	return n;
 }
 
-void parse_lut_entry(const char *buf, float dst[3])
+void parse_lut_entry(std::string_view buf, float dst[3])
 {
 	buf = parse_number(buf, dst + 0);
 	buf = skip_space(buf);
@@ -160,6 +151,7 @@ Cube read_cube_from_file(const char *path)
 #endif
 	std::FILE *file = file_uptr.get();
 	char buf[LINE_LEN];
+	std::string_view sv;
 
 	if (!file)
 		throw_system_error();
@@ -169,32 +161,33 @@ Cube read_cube_from_file(const char *path)
 
 	while (true) {
 		read_line(buf, file);
+		sv = buf;
 
-		if (is_keyword(buf, "TITLE")) {
+		if (is_keyword(sv, "TITLE")) {
 			try {
-				cube.title = parse_title(buf);
+				cube.title = parse_title(sv);
 			} catch (...) {
 				// Non-fatal.
 			}
-		} else if (is_keyword(buf, "DOMAIN_MIN")) {
-			parse_domain_minmax(buf, cube.domain_min);
-		} else if (is_keyword(buf, "DOMAIN_MAX")) {
-			parse_domain_minmax(buf, cube.domain_max);
-		} else if (is_keyword(buf, "LUT_1D_SIZE")) {
+		} else if (is_keyword(sv, "DOMAIN_MIN")) {
+			parse_domain_minmax(sv, cube.domain_min);
+		} else if (is_keyword(sv, "DOMAIN_MAX")) {
+			parse_domain_minmax(sv, cube.domain_max);
+		} else if (is_keyword(sv, "LUT_1D_SIZE")) {
 			if (has_lut_size)
 				throw std::runtime_error{ "duplicate LUT declaration" };
 
-			cube.n = parse_lut_size(buf);
+			cube.n = parse_lut_size(sv);
 			cube.is_3d = false;
 			has_lut_size = true;
-		} else if (is_keyword(buf, "LUT_3D_SIZE")) {
+		} else if (is_keyword(sv, "LUT_3D_SIZE")) {
 			if (has_lut_size)
 				throw std::runtime_error{ "duplicate LUT declaration" };
 
-			cube.n = parse_lut_size(buf);
+			cube.n = parse_lut_size(sv);
 			cube.is_3d = true;
 			has_lut_size = true;
-		} else if (std::isdigit(buf[0], std::locale::classic()) || buf[0] == '+' || buf[0] == '-' || buf[0] == '.') {
+		} else if ("0123456789+-."sv.find(sv.front()) != std::string_view::npos) {
 			break;
 		}
 	}
@@ -210,13 +203,14 @@ Cube read_cube_from_file(const char *path)
 	size_t size = lut_size(cube.n, cube.is_3d);
 
 	cube.lut.insert(cube.lut.end(), 3, 0.0f);
-	parse_lut_entry(buf, &*(cube.lut.end() - 3));
+	parse_lut_entry(sv, &*(cube.lut.end() - 3));
 
 	for (unsigned i = 1; i < size; ++i) {
 		read_line(buf, file);
+		sv = buf;
 
 		cube.lut.insert(cube.lut.end(), 3, 0.0f);
-		parse_lut_entry(buf, &*(cube.lut.end() - 3));
+		parse_lut_entry(sv, &*(cube.lut.end() - 3));
 	}
 
 	return cube;
